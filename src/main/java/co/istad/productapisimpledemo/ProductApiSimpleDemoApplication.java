8724331ed4b9045344cc73d75192569860dbbbcb
Seang Malin <seangmalin22@gmail.com>
2026-06-05 11:03:11 +0700

    initial commit

---
+++ src/main/java/co/istad/productapisimpledemo/ProductApiSimpleDemoApplication.java
@@ -0,0 +1,14 @@
+package co.istad.productapisimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiSimpleDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductApiSimpleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/istad/productapisimpledemo/ProductApiSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.productapisimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiSimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
